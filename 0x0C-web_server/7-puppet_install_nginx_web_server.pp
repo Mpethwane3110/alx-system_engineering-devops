3c607014b219efe911fc5e7ec76a677dcf194c08
Mpethwane3110 <Mpethwanenomfundo@gmail.com>
2023-11-29 19:21:01 -0800

    server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,46 +1,42 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Configure server using puppet
 
-# 1- Install Nginx
-sudo apt-get update
-sudo apt-get install -y nginx
+# defines a Puppet class called nginx_server that 
+#  encapsulates the configuration for the Nginx server.
+class nginx_server {
+  package { 'nginx':
+    ensure => installed,
+  }
 
-# 2- Check Nginx configuration and fix errors
-sudo nginx -t
-sudo systemctl reload nginx
+#  manages the Nginx service.
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    require => Package['nginx'],
+  }
+# manages the Nginx configuration file located at /etc/nginx/sites-available/default.
+  file { '/etc/nginx/sites-available/default':
+    ensure  => present,
+    content => "
+      server {
+        listen      80 default_server;
+        listen      [::]:80 default_server;
+        root        /var/www/html;
+        index       index.html index.htm;
 
-# 3- Create the 404 not found page configuration file
-sudo echo "
-server {
-    listen 80;
-    server_name _;
+        location / {
+          return 200 'Hello World!';
+        }
 
-    error_page 404 /404_custom_page.html;
-    location = /404_custom_page.html {
-        internal;
-    }
+        location /redirect_me {
+          return 301 http://cuberule.com/;
+        }
+      }
+    ",
+    notify => Service['nginx'],
+  }
 }
-" | sudo tee /etc/nginx/sites-available/404_custom_page
+#  includes the nginx_server class, ensuring that it gets applied.
+include nginx_server
 
-# Create a symbolic link to the sites-enabled directory
-sudo ln -s /etc/nginx/sites-available/404_custom_page /etc/nginx/sites-enabled/
 
-# Create the custom 404 page file
-sudo echo "
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>404 Not Found</title>
-    </head>
-    <body>
-        <h1>Ceci n'est pas une page</h1>
-    </body>
-</html>
-" | sudo tee /var/www/html/404_custom_page.html
-
-# Restart nginx to apply changes
-sudo service nginx restart
-
-# Output the instructions to the user
-echo "Nginx 404 custom page configuration complete."
-echo "To verify the 404 custom page, visit http://<your_server_IP>/some_nonexistent_page in your web browser."
-echo "The browser should display the custom 404 page with the string 'Ceci n'est pas une page'."
