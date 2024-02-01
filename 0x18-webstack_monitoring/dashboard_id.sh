d391a1c7a446953b3dd57f1d3b261a10b6a76aa2
Mpethwane3110 <Mpethwanenomfundo@gmail.com>
2024-01-31 23:10:46 -0800

    webstack

--- 0x18-webstack_monitoring/dashboard_id.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Requests Datadog API to get dashboard info, incl ID
-api_key="23b8278957442b9b2a396fc2cd10c98a"
-
-curl -X GET \
--H "DD-API-KEY: ${api_key}" \
--H "DD-APPLICATION-KEY: ${app_key}" \
-"https://api.datadoghq.com/api/v1/dashboard"
